ee7d737c42aaaf8f1116a68db340da9c15669099
gpbsumido <gpbsumido@gmail.com>
2025-04-16 19:45:01 -0400

    add headers

--- server.js
+++ server.js
@@ -12,7 +12,21 @@ app.use(cors());
 app.get("/api/nba/teams", async (req, res) => {
     try {
         const url = "https://stats.nba.com/stats/leaguestandingsv3?LeagueID=00&Season=2024-25&SeasonType=Regular+Season";
-        const response = await fetch(url);
+        const response = await fetch(url, {
+            headers: {
+                Host: "stats.nba.com",
+                "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+                Accept: "*/*",
+                "Accept-Language": "en-US,en;q=0.9",
+                "Accept-Encoding": "gzip, deflate, br",
+                "x-nba-stats-origin": "stats",
+                "x-nba-stats-token": "true",
+                Connection: "keep-alive",
+                Referer: "https://www.nba.com/",
+                Origin: "https://www.nba.com",
+                "Cache-Control": "max-age=0",
+            }
+        });
         const data = await response.json();
 
         //if (!data.resultSets?.[0]?.rowSet) {
