ef30205d86a9e18714727284ef54621e06cbb73e
gpbsumido <gpbsumido@gmail.com>
2025-04-16 19:33:53 -0400

    put back data

--- server.js
+++ server.js
@@ -11,8 +11,25 @@ app.use(cors());
 // NBA API Proxy endpoint
 app.get("/api/nba/teams", async (req, res) => {
     try {
-        console.log('Starting function');
-        return res.status(200).json({ data: 'Hi' });
+        const url = "https://stats.nba.com/stats/leaguestandingsv3?LeagueID=00&Season=2024-25&SeasonType=Regular+Season";
+        const response = await fetch(url);
+        const data = await response.json();
+
+        //if (!data.resultSets?.[0]?.rowSet) {
+        //    return res.status(500).json({ error: "Failed to fetch teams." });
+        //}
+
+        //const teams = data.resultSets[0].rowSet.map((row) => ({
+        //    id: parseInt(row[2]),
+        //    name: row[4],
+        //    full_name: `${row[3]} ${row[4]}`,
+        //    abbreviation: row[5],
+        //    city: row[3],
+        //    conference: row[6],
+        //    division: row[10]
+        //}));
+
+        return res.status(200).json({ data: data });
     } catch (error) {
         return res.status(500).json({ error: "Failed to fetch teams", details: error.message });
     }
