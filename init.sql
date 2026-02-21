5df3ed59063cf062c7c7f50347372e5dcff2f7c9
gpbsumido <gpbsumido@gmail.com>
2026-02-21 15:18:15 -0700

    add missing table defs for some tables

--- init.sql
+++ init.sql
@@ -16,6 +16,42 @@ CREATE TABLE IF NOT EXISTS postforum (
     created_at TIMESTAMP WITH TIME ZONE DEFAULT CURRENT_TIMESTAMP
 );
 
+-- Create gallery table
+CREATE TABLE IF NOT EXISTS gallery (
+    id SERIAL PRIMARY KEY,
+    title TEXT NOT NULL,
+    description TEXT NOT NULL,
+    image_url TEXT NOT NULL,
+    date TIMESTAMP WITH TIME ZONE NOT NULL,
+    user_sub TEXT
+);
+
+-- Create med_journal table
+CREATE TABLE IF NOT EXISTS med_journal (
+    id UUID PRIMARY KEY,
+    "patientSetting" TEXT NOT NULL,
+    "interaction" TEXT NOT NULL,
+    "canmedsRoles" JSONB,
+    "learningObjectives" JSONB,
+    "rotation" TEXT,
+    "date" DATE,
+    "location" TEXT,
+    "hospital" TEXT,
+    "doctor" TEXT,
+    "whatIDidWell" TEXT,
+    "whatICouldImprove" TEXT,
+    user_sub TEXT NOT NULL
+);
+
+-- Create feedback table
+CREATE TABLE IF NOT EXISTS feedback (
+    id UUID PRIMARY KEY,
+    text TEXT NOT NULL,
+    rotation TEXT NOT NULL,
+    journal_entry_id UUID REFERENCES med_journal(id),
+    user_sub TEXT NOT NULL
+);
+
 -- Grant privileges
 GRANT ALL PRIVILEGES ON ALL TABLES IN SCHEMA public TO postgres;
-GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA public TO postgres; 
+GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA public TO postgres;
